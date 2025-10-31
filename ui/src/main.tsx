c33fd343f2c4460b082bd92f527e41ec9c7bc9c6
Arie <56198768+heyebai@users.noreply.github.com>
2025-10-31 21:24:41 +1000

    feat(ui): init

---
+++ ui/src/main.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import App from "./App";
+import "./index.css";
+import { store } from "./app/store";
+import { queryClient } from "./app/queryClient";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </Provider>
+  </React.StrictMode>
+);
